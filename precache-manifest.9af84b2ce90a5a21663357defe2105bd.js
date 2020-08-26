0fff0054244533d990bef27dce2ac983a911684a
rajababusaikia <rajababu.saikia@venuemonk.com>
2020-08-26 17:31:52 +0530

    Updates

---
+++ precache-manifest.9af84b2ce90a5a21663357defe2105bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b891c7a4807fb9dea7f7ad9568f128cf",
+    "url": "/React-todo-basic/index.html"
+  },
+  {
+    "revision": "dc307bd217c7576b0930",
+    "url": "/React-todo-basic/static/css/main.f171ecbb.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/React-todo-basic/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "dc307bd217c7576b0930",
+    "url": "/React-todo-basic/static/js/main.dcc95905.chunk.js"
+  },
+  {
+    "revision": "f20904c682d2da5b70de",
+    "url": "/React-todo-basic/static/js/runtime~main.549db757.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo-basic/static/media/logo.5d5d9eef.svg"
+  }
+]);
